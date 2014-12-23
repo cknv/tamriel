101613ee0772e6b4e413a6590ea88d5444e2f631
Esben Sonne <esbensonne@gmail.com>
2014-12-23 22:46:44 +0100

    Added script to generate theme file.
    The theme differs by small amounts.

---
+++ build.py
@@ -0,0 +1,157 @@
+from theme import theme
+
+
+class Color:
+    def __init__(self, light, medium, dark):
+        self.light = light
+        self.medium = medium
+        self.dark = dark
+
+# Tango colors used
+butter = Color('#fce94f', '#edd400', '#c4a000')
+orange = Color('#fcaf3e', '#f57900', '#ce5c00')
+chocolate = Color('#e9b96e', '#c17d11', '#8f5902')
+chameleon = Color('#8ae234', '#73d216', '#4e9a06')
+sky_blue = Color('#729fcf', '#3465a4', '#204a87')
+plum = Color('#ad7fa8', '#75507b', '#5c3566')
+scarlet_red = Color('#ef2929', '#cc0000', '#a40000')
+aluminium_light = Color('#eeeeec', '#d3d7cf', '#babdb6')
+aluminium_dark = Color('#888a85', '#555753', '#2e3436')
+
+
+if __name__ == '__main__':
+    style = {
+        'author': 'Esben Sonne',
+        'name': 'tamriel',
+        'version': '0.1.1',
+        'uuid': '13E579BF-40AB-42E2-9EAB-0AD3EDD88532',
+        'colorSpaceName': 'sRGB',
+        'semanticClass': 'theme.dark.tamriel',
+        'settings': {
+            'invisibles': '#3B3A32',
+            'lineHighlight': '#31221B',
+            'selection': '#49483E',
+            'findHighlight': '#FFE792',
+            'findHighlightForeground': '#000000',
+            'selectionBorder': '#222218',
+            'activeGuide': '#9D550FB0',
+            'bracketsForeground': '#F8F8F2A5',
+            'bracketsOptions': 'underline',
+            'bracketContentsForeground': '#F8F8F2A5',
+            'bracketContentsOptions': 'underline',
+            'tagsOptions': 'stippled_underline',
+            'background': '#120F0A',
+            'caret': aluminium_light.light,
+            'foreground': aluminium_light.light,
+            'gutterForeground': '#73797b75',
+            'guide': '#73797b30',
+        },
+        'style': [
+            {
+                'name': 'comment',
+                'scope': 'comment',
+                'foreground': aluminium_dark.medium,
+                'fontStyle': 'italic',
+            },
+            {
+                'name': 'constant',
+                'scope': 'constant',
+                'foreground': scarlet_red.medium,
+            },
+            {
+                'name': 'constant numeric',
+                'scope': 'contant.numeric',
+                'foreground': '#ffd000',
+            },
+            {
+                'name': 'string',
+                'scope': 'string',
+                'foreground': '#ffd000',
+            },
+            {
+                'name': 'name',
+                'scope': 'storage.type',
+                'fontStyle': 'bold'
+            },
+            {
+                'name': 'class',
+                'scope': 'entity.name.type.class',
+                'fontStyle': 'bold',
+                'foreground': aluminium_light.light,
+            },
+            {
+                'name': 'function',
+                'scope': 'entity.name',
+                'fontStyle': 'bold',
+            },
+            {
+                'name': 'inherired from',
+                'scope': 'entity.other.inherited-class',
+                'foreground': chameleon.dark,
+            },
+            {
+                'name': 'keyword',
+                'scope': 'keyword',
+                'fontStyle': 'bold',
+                'foreground': aluminium_light.medium,
+            },
+            {
+                'name': 'variable',
+                'scope': 'variable',
+                'foreground': sky_blue.light,
+            },
+            {
+                'name': 'builtin functions',
+                'scope': 'support.function',
+                'foreground': sky_blue.medium,
+            },
+            {
+                'name': 'builtin types',
+                'scope': 'support.type',
+                'foreground': sky_blue.medium,
+            },
+            {
+                'name': 'magic methods',
+                'scope': 'support.function.magic',
+                'foreground': orange.medium,
+            },
+            {
+                'name': 'magic names',
+                'scope': 'support.variable.magic',
+                'foreground': sky_blue.medium,
+            },
+            {
+                'name': 'decorators',
+                'scope': 'support.function.decorator',
+                'foreground': aluminium_dark.light,
+            },
+            {
+                'name': 'xml-tags',
+                'scope': 'entity.name.tag',
+                'foreground': sky_blue.dark,
+            },
+            # gitgutter colors.
+            {
+                'name': 'gitgutter deleted',
+                'scope': 'markup.deleted.git_gutter',
+                'foreground': scarlet_red.medium,
+            },
+            {
+                'name': 'gitgutter inserted',
+                'scope': 'markup.inserted.git_gutter',
+                'foreground': chameleon.medium,
+            },
+            {
+                'name': 'gitgutter changed',
+                'scope': 'markup.changed.git_gutter',
+                'foreground': butter.medium,
+            }
+        ],
+    }
+
+    output = theme(style)
+
+    from zipfile import ZipFile
+    with ZipFile('tamriel.sublime-package', 'w') as fo:
+        fo.writestr('tamriel.tmTheme', output.encode('utf-8'))
+
---
+++ theme.py
@@ -0,0 +1,82 @@
+from lxml.etree import Element, tostring
+
+
+def key_node(name):
+    node = Element('key')
+    node.text = name
+    return node
+
+
+def string_node(value):
+    node = Element('string')
+    node.text = value
+    return node
+
+
+def style_node(style):
+    node = Element('dict')
+    node.append(key_node('name'))
+    node.append(string_node(style['name']))
+    node.append(key_node('scope'))
+    node.append(string_node(style['scope']))
+    node.append(key_node('settings'))
+    settings = Element('dict')
+
+    if 'foreground' in style:
+        settings.append(key_node('foreground'))
+        settings.append(string_node(style['foreground']))
+    if 'fontStyle' in style:
+        settings.append(key_node('fontStyle'))
+        settings.append(string_node(style['fontStyle']))
+
+    node.append(settings)
+    return node
+
+
+def add_node(name, style, parent):
+    parent.append(key_node(name))
+    parent.append(string_node(style[name]))
+
+
+def general_settings(style):
+    settings = Element('array')
+    sub = Element('dict')
+    sub.append(key_node('settings'))
+    settings.append(sub)
+    sub2 = Element('dict')
+    for key, value in style['settings'].items():
+        sub2.append(key_node(key))
+
+        sub2.append(string_node(value))
+
+    sub.append(sub2)
+    for each in style['style']:
+        settings.append(style_node(each))
+    return settings
+
+
+def theme(style):
+    root = Element('plist', version=style['version'])
+
+    doctype = '<!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">'
+    settings = Element('dict')
+    root.append(settings)
+
+    add_node('author', style, settings)
+    add_node('name', style, settings)
+
+    settings.append(key_node('settings'))
+    settings.append(general_settings(style))
+
+    add_node('uuid', style, settings)
+    add_node('colorSpaceName', style, settings)
+    add_node('semanticClass', style, settings)
+
+
+    return tostring(
+        root,
+        pretty_print=True,
+        doctype=doctype,
+        xml_declaration=True,
+        encoding='UTF-8',
+    ).decode('utf-8')
